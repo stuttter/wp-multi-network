47bb7db7338ed8cc8ec210b503ac353a7599273f
John James Jacoby <johnjamesjacoby@me.com>
2016-02-08 03:37:23 -0600

    Refactor `wp_validate_site_url()` a bit.
    It's more forgiving, but I don't think too much so.

--- includes/compat.php
+++ includes/compat.php
@@ -48,7 +48,7 @@ function wp_sanitize_site_path( $path = '' ) {
 }
 endif;
 
-if ( function_exists( 'wp_validate_site_url' ) ) :
+if ( ! function_exists( 'wp_validate_site_url' ) ) :
 /**
  * Is a site URL okay to save?
  *
@@ -62,31 +62,25 @@ if ( function_exists( 'wp_validate_site_url' ) ) :
  *
  * @return boolean
  */
-function wp_validate_site_url( $domain, $path, $slug = '' ) {
+function wp_validate_site_url( $domain, $path, $site_id = 0 ) {
 	global $wpdb;
 
-	// Bail if empty
-	if ( empty( $slug ) ) {
+	// Does domain exist on this network
+	$exists = domain_exists( $domain, $path, get_current_site()->id );
+
+	// Bail if domain is current site ID
+	if ( $exists === $site_id ) {
+		return true;
+	}
+
+	// Bail if domain exists and it's not this site
+	if ( true === $exists ) {
 		return false;
 	}
 
-	// Bail if not lowercase or numbers
-	if ( preg_match( '/[^a-z0-9]+/', $slug ) ) {
-		return false;
-	}
-
-	// All numeric?
-	if ( preg_match( '/^[0-9]*$/', $slug ) ) {
-		return false;
-	}
-
-	// Bail if less than 4 chars
-	if ( strlen( $slug ) < 4 ) {
-		return false;
-	}
-
-	// Bail if domain exists on this network
-	if ( domain_exists( $domain, $path, get_current_site()->id ) ) {
+	// Bail if site is in signups table
+	$signup = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM {$wpdb->signups} WHERE domain = %s AND path = %s", $domain, $path ) );
+	if ( ! empty( $signup ) ) {
 		return false;
 	}
 
@@ -95,38 +89,63 @@ function wp_validate_site_url( $domain, $path, $slug = '' ) {
 		return true;
 	}
 
-	// Get illegal names
-	$illegal_names = get_site_option( 'illegal_names' );
+	// Get pieces of domain & path
+	$paths   = explode( '/', $path );
+	$domains = substr_count( $domain, '.' ) > 1
+		? (array) substr( $domain, 0, strpos( $domain, '.' ) )
+		: array();
+	$pieces  = array_filter( array_merge( $domains, $paths ) );
 
-	// Maybe merge reserved names
-	if ( ! is_subdomain_install() ) {
-		$illegal_names = array_merge( $illegal_names, get_subdirectory_reserved_names() );
-	}
+	// Loop through pieces
+	foreach ( $pieces as $slug ) {
 
-	// Bail if contains illegal names
-	if ( in_array( $slug, $illegal_names, true ) ) {
-		return false;
-	}
-
-	// Bail if username exists
-	if ( username_exists( $slug ) ) {
-		return false;
-	}
-
-	// Bail if subdirectory install and page exists on primary site of network
-	if ( ! is_subdomain_install() ) {
-		switch_to_blog( get_current_site()->blog_id );
-		$page = get_page_by_path( $slug );
-		restore_current_blog();
-		if ( ! empty( $page ) ) {
+		// Bail if empty
+		if ( empty( $slug ) ) {
 			return false;
 		}
-	}
 
-	// Bail if site is in signups table
-	$signup = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM {$wpdb->signups} WHERE domain = %s AND path = %s", $domain, $path ) );
-	if ( ! empty( $signup ) ) {
-		return false;
+		// Bail if not lowercase or numbers
+		if ( preg_match( '/[^a-z0-9]+/', $slug ) ) {
+			return false;
+		}
+
+		// All numeric?
+		if ( preg_match( '/^[0-9]*$/', $slug ) ) {
+			return false;
+		}
+
+		// Bail if less than 4 chars
+		if ( strlen( $slug ) < 3 ) {
+			return false;
+		}
+
+		// Get illegal names
+		$illegal_names = get_site_option( 'illegal_names' );
+
+		// Maybe merge reserved names
+		if ( ! is_subdomain_install() ) {
+			$illegal_names = array_merge( $illegal_names, get_subdirectory_reserved_names() );
+		}
+
+		// Bail if contains illegal names
+		if ( in_array( $slug, $illegal_names, true ) ) {
+			return false;
+		}
+
+		// Bail if username exists
+		if ( username_exists( $slug ) ) {
+			return false;
+		}
+
+		// Bail if subdirectory install and page exists on primary site of network
+		if ( ! is_subdomain_install() ) {
+			switch_to_blog( get_current_site()->blog_id );
+			$page = get_page_by_path( $slug );
+			restore_current_blog();
+			if ( ! empty( $page ) ) {
+				return false;
+			}
+		}
 	}
 
 	// Okay, s'all good
