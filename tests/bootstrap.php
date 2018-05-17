9bb6cc0fc02ee30ff5ae031ac1fcf2a920cf2559
Jonny Harris <jonathan_harris@ipcmedia.com>
2018-05-17 20:49:21 +0100

    Improvements to tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,31 +1,33 @@
 <?php
 /**
- * Bootstrap the plugin unit testing environment.
+ * PHPUnit bootstrap file
  *
- * @package WordPress
- * @subpackage JSON API
+ * @package Testing1
  */
 
-// Support for:
-// 1. `WP_DEVELOP_DIR` environment variable
-// 2. Plugin installed inside of WordPress.org developer checkout
-// 3. Tests checked out to /tmp
-if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
-	$test_root = getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit';
-} else if ( file_exists( '../../../../tests/phpunit/includes/bootstrap.php' ) ) {
-	$test_root = '../../../../tests/phpunit';
-} else if ( file_exists( '/tmp/wordpress-tests-lib/includes/bootstrap.php' ) ) {
-	$test_root = '/tmp/wordpress-tests-lib';
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+
+if ( ! $_tests_dir ) {
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
 
-require $test_root . '/includes/functions.php';
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL;
+	exit( 1 );
+}
 
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
+
+/**
+ * Manually load the plugin being tested.
+ */
 function _manually_load_plugin() {
 	require dirname( dirname( __FILE__ ) ) . '/wpmn-loader.php';
 }
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin', 0 );
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-// WP Multi-Network must be multisite
-define( 'WP_TESTS_MULTISITE', true );
+// Start up the WP testing environment.
+require $_tests_dir . '/includes/bootstrap.php';
 
-require $test_root . '/includes/bootstrap.php';
+
